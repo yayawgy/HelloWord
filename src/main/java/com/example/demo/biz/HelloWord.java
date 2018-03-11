781d6e9e8ac52fc5d94eaf875274a2860b09c21b
yayawgy <1037414002@qq.com>
2018-03-11 23:12:36 +0800

    as

---
+++ src/main/java/com/example/demo/biz/HelloWord.java
@@ -0,0 +1,13 @@
+package com.example.demo.biz;
+
+/**
+ * Created by lenovo on 2018/3/11.
+ */
+public class HelloWord {
+
+    public static void main(String[] args){
+        System.out.print("hello wrod !!!");
+    }
+
+
+}
